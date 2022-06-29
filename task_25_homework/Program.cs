1a31810ab5612662521df3d9a99644b566fd8c0e
deardaddy <sergpomorsky@gmail.com>
2022-06-29 16:25:08 +0300

    Add task 25

--- task_25_homework/Program.cs
+++ task_25_homework/Program.cs
@@ -7,7 +7,7 @@ Console.WriteLine("Введите число A");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число B");
 int B = Convert.ToInt32(Console.ReadLine());
-int sum = 1;
+int sum = 1; // 
 for (int i = 1; i <= B; i++)
 {
 sum = sum*A;
---
+++ task_29_homework/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+//и выводит их на экран.
+int [] array = new int [8];
+{Console.Write($"[");
+for(int i = 0; i <= array.Length - 1; i++)
+    {
+    array[i] = new Random(). Next(0, 99);
+    Console.Write($"{array[i]},"); 
+    }
+    Console.Write($"]");
+}
+Console.WriteLine();
+
